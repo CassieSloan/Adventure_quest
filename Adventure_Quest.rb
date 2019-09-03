99ede0105bd2cae986b012b55d5fc2d9febf0154
Cassandra sloan <Cassandra@Cassandras-MacBook-Pro.local>
2019-09-03 11:42:02 +1000

    second commit. added greg dissapear

--- Adventure_Quest.rb
+++ Adventure_Quest.rb
@@ -1,4 +1,7 @@
                                                   
+require 'colorize'
+
+
 puts "█████╗ ██████╗ ██╗   ██╗███████╗███╗   ██╗████████╗██╗   ██╗██████╗ ███████╗     ██████╗ ██╗   ██╗███████╗███████╗████████╗"
 puts "██╔══██╗██╔══██╗██║   ██║██╔════╝████╗  ██║╚══██╔══╝██║   ██║██╔══██╗██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔════╝╚══██╔══╝"
 puts "███████║██║  ██║██║   ██║█████╗  ██╔██╗ ██║   ██║   ██║   ██║██████╔╝█████╗      ██║   ██║██║   ██║█████╗  ███████╗   ██║   "
@@ -23,10 +26,11 @@ end
 end
 
 puts "Hail and well met, traveller!"
+puts "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
 puts "1. Um...who are you?"
 puts "2. Actually, my bus broke down – #Transportnsw did me dirty once again"
 puts "3. I AM READY FOR AN EPIC ADVENTURE!!"
-
+puts "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
 
 while true
     welcome_choice = gets.chomp
@@ -45,3 +49,24 @@ while true
     next
 end
 end
+
+puts "In any case, may I ask your name?"
+
+name = gets.chomp
+  
+puts "Welcome, #{name}. I hope you enjoy your time here. Make good choices!"
+puts "✧･ﾟ: *✧･ﾟ:* 　　 *:･ﾟ✧*:･ﾟ✧".colorize(:light_blue )
+puts ".・゜゜・　　・゜゜・．゜・".colorize(:light_blue )
+puts "゜゜・✧*: P O O F ! ・゜゜✧*:".colorize(:light_blue )
+puts "*✧･ﾟ:* 　゜゜・　 *:・．゜・ ".colorize(:light_blue )
+puts" :* 　　 *:･ﾟ✧*:･゜゜・　　・゜".colorize(:light_blue )
+puts " * * * * * * * * * * * * * * * * * * * * * * * "
+puts " *  G R E G  H A S  D I S S A P E A R E D!   * "
+puts " * * * * * * * * * * * * * * * * * * * * * * * "
+puts "psst, it's me Greg. I'm talking to you from the Astral plane. Press 'enter' to continue through your journey."
+puts "#{name}: Well, that sure was weird. He reminded me of my uncle somehow."
+puts "#{name}: Now, where should I go?"
+puts "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+puts ""
+puts ""
+puts ""
