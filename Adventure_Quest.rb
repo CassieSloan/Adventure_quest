d5c2399c4f7dcf48fbd3044d0b89449c82eb0baa
Cassandra sloan <Cassandra@Cassandras-MacBook-Pro.local>
2019-09-03 12:31:39 +1000

    wrapped up intro, writing methods to seperate files - bar, forest and town

--- Adventure_Quest.rb
+++ Adventure_Quest.rb
@@ -36,7 +36,7 @@ continue
 
 ################################## T H E  B E G I N N I N G ##################################
 
-puts "Hail and well met, traveller!"
+puts "Greg: Hail and well met, traveller!"
 puts ""
 puts "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
 puts "1. Um...who are you?"
@@ -48,13 +48,13 @@ while true
     welcome_choice = gets.chomp
     case welcome_choice
     when "1"
-      puts "Nosey, aren't we? I am a nameless and faceless figment from the Astral plane with power incomprable to the meat-socks of this realm, but here in my cooporeal form – I guess you can call me Greg."
+      puts "Greg: Nosey, aren't we? I am a nameless and faceless figment from the Astral plane with power incomprable to the meat-socks of this realm, but here in my cooporeal form – I guess you can call me Greg."
       break
     when "2"
-      puts "Oh ho ho! Yes, I'm well aquainted with the not-sp-secret society of chaotic lizard overlords that form the Transport NSW clan. You know they make a mean squewered rat?"
+      puts "Greg: Oh ho ho! Yes, I'm well aquainted with the not-so-secret society of chaotic lizard overlords that form the Transport NSW clan. You know they make a mean squewered rat?"
       break
     when "3"
-      puts "Wonderful! You're the first person to tell me that all week. It seems the capitalist regions surrounding this land have taken the sense of wonder and magic from their people."
+      puts "Greg: Wonderful! You're the first person to tell me that all week. It seems the capitalist regions surrounding this land have taken the sense of wonder and magic from their people."
      break
     else 
        puts "Hey genius, press 1, 2 or 3!"
@@ -83,11 +83,35 @@ continue
 puts "#{name}: Well, that sure was weird. He reminded me of my uncle somehow."
 continue
 puts "#{name}: Now, where should I go?"
+
 puts "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
-puts "1."
-puts "2."
-puts "3."
+puts "1. The bar!"
+puts "2. The town!"
+puts "3. The forest! CASSIE WRITE OTHER TWO FIRST AND SEE IF WE HAVE TIME FOR ANOTHER. Dont get ahead of urself u excitable bish"
 puts "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+"
+
+while true
+where_to_go = gets.chomp
+case where_to_go
+  when "1"
+    puts "I'm pretty thirsty – let's head to the bar"
+    break
+  when "2"
+    puts "I'm feeling some local culture, let's head to the markets"
+    break
+  when "3"
+    puts "I've been couped up in the city for too long, lets get some fresh air!"
+  break
+  else 
+    puts "Hey genius, press 1, 2 or 3!"
+  next
+  end
+end
+
+  continue
 
 
 ################################## A D V E N T U R E  S T A R T S ##################################
+
+#**bar methods
+#** town methods
